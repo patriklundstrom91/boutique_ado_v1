6b34b1739b7aec859f276c49925802c64752025a
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-06-16 21:23:04 +0200

    added forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -54,6 +54,7 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
+
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
