ed68c6a383570e1bb747e0c7877fa588fd8e3846
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-06-23 15:12:02 +0200

    added and tested webhooks and handlers

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-8y+l9z7rv6_)agvc$@f$73z_1m32ib-z5@x(gj3#l$y4-w$+dj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'localhost',]
 
 
 # Application definition
@@ -175,6 +176,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
